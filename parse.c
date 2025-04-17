30b47a0fec2964a44c51a5bbaa003250a6645975
dob24 <dob24@njit.edu>
2025-04-16 23:02:47 -0400

    added resources dir and resources markdown

--- parse.c
+++ parse.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <string.h>
+#include "mpc.h"
+#include <editline/readline.h>
+#include <histedit.h>
 
 static char buffer[2048];
 
-/* Fake readline function */
 char* readline(char* prompt) {
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
@@ -15,30 +13,52 @@ char* readline(char* prompt) {
   return cpy;
 }
 
-/* Fake add_history function */
 void add_history(char* unused) {}
 
-/* Otherwise include the editline headers */
-#else
-#include <editline/readline.h>
-#include <editline/history.h>
-#endif
 
 int main(int argc, char** argv) {
-   
-  puts("Lispy Version 0.0.0.0.1");
+  
+  /* Create Some Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+  
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lispy);
+  
+  puts("Lispy Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
-   
+  
   while (1) {
-    
-    /* Now in either case readline will be correctly defined */
+  
     char* input = readline("lispy> ");
     add_history(input);
-
-    printf("No you're a %s\n", input);
-    free(input);
     
+    /* Attempt to parse the user input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      /* On success print and delete the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise print and delete the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+    
+    free(input);
   }
+  
+  /* Undefine and delete our parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   
   return 0;
 }
---
+++ resources.md
@@ -0,0 +1,7 @@
+This is a list of resources and references i'm using to create this project
+
+
+
+1.) writing an inerpreter in Go book by 
+2.) [writing-a-C-interpreter] (https://github.com/lotabout/write-a-C-interpreter)
+
